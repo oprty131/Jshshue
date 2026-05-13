af01a4d19d3ff944c818b7c5e7e0a99f968afb88
oprty131 <slwmplylw@gmail.com>
2026-05-13 22:44:36 +0300

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,11 @@
 export default {
   async fetch(request, env) {
+    if (request.headers.get("Upgrade") === "websocket") {
+      const id = env.CHAT.idFromName("global");
+      const stub = env.CHAT.get(id);
+      return stub.fetch(request);
+    }
+
     const id = env.CHAT.idFromName("global");
     const stub = env.CHAT.get(id);
     return stub.fetch(request);
